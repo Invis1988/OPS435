9ee29bf123e015f4472eda6d522d6808218aaf57
Invis1988 <kieroconnor@gmail.com>
2017-04-01 01:17:23 -0400

    Add files via upload

---
+++ zodiac.bash
@@ -0,0 +1,66 @@
+#!/bin/bash
+function chinese {
+eyear=$(( year - 60 ))
+query="$eyear$2$3"
+while IFS=, read -r term start end; do
+	((query >= start)) && ((query <= end)) && echo -n "Your Chinese Zodiac is: $term"
+done <chzod
+query="$1$2$3"
+while IFS=, read -r term start end; do
+	((query >= start)) && ((query <= end)) && echo " $term"
+done <animal
+}
+case $# in 
+   1) year=${1:0:4}
+	month=${1:4:2}
+   	   day=${1:6:2} ;;
+   3) year=$1
+	   month=$2
+	     day=$3 ;;
+
+     6) if [[ $1 = "-y" ]]; then
+	    year=$2
+    elif [[ $3 = "-y" ]]; then
+	   year=$4
+   elif [[ $5 = "-y" ]]; then
+	  year=$6
+  fi
+	if [[ $1 = "-m" ]]; then
+	    month=$2
+    elif [[ $3 = "-m" ]]; then
+	   month=$4
+   elif [[ $5 = "-m" ]]; then
+	  month=$6
+  fi
+if [[ $1 = "-d" ]]; then
+	    day=$2
+    elif [[ $3 = "-d" ]]; then
+	   day=$4
+   elif [[ $5 = "-d" ]]; then
+	  day=$6
+  fi ;;
+  *) echo "USAGE: yyyymmdd, yyyy mm dd, or specify -y -m -d"
+esac
+`date -d $year$month$day >/dev/null 2>/dev/null`
+test $? -eq 1 && echo "USAGE: yyyymmdd, yyyy mm dd, or specify -y -m -d" && exit 1
+wmonth=$(date -d $year$month$day +%-m | sed 's/ //g')
+query="$wmonth$day"
+while IFS=, read -r term start end; do
+	((query >= start)) && ((query <= end)) && echo "Your Western Zodiac is $term"
+done <west
+range=({1984..2043})
+if [[ ! "${range[@]}" =~ "$year" ]];then
+	echo "Year must be between 1984 and 2043 for Chinese Zodiac"
+exit
+fi
+eyear=$((year - 60))
+query="$eyear$month$day"
+while IFS=, read -r term start end; do
+	((query >= start)) && ((query <= end)) && echo -n "Your Chinese Zodiac is $term"
+done <chzod
+query="$year$month$day"
+while IFS=, read -r term start end; do
+	((query >= start)) && ((query <= end)) && echo " $term"
+done <animal
+echo "You were born on a $(date -d $year$month$day +%A)"
+echo -e "Here's your horoscope:\n `fortune`"
